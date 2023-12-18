3d9c64e1a30eb7649400939bb33b81ea768afed0
NASHirul27 <nashirul.alhaq@gmail.com>
2023-12-18 08:48:52 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "F55122043_modul 5"
+include(":app")
+ 
